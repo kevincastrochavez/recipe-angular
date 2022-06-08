f66e41ced773d9b3e4887cc9c510a6e2e8078b2d
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-08 11:15:01 -0600

    reset the form and switched editMode

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -43,6 +43,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
+
+    this.editMode = false;
+    form.reset();
   }
 
   ngOnDestroy() {
